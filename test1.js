1d846a3d51e80fe26a05a3439659d8b7d0b37485
elephent <elephent@xj.com>
2016-08-02 14:44:54 +0800

    add test1 a sample

--- test1.js
+++ test1.js
@@ -1,2 +1,12 @@
 'use strict';
-console.log('Hello World');
+
+var fs=require('fs');
+fs.readFile('sample.txt','utf-8',function(err,data){
+	if(err)
+		{console.log(err);}
+	else
+		{console.log(data);}
+	
+}
+
+);
